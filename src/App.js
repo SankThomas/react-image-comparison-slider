b55a46e1c392179f4f19b7adec279980b6cd15c2
Thomas Sankara <thomassankara74@gmail.com>
2021-02-02 10:39:26 +0300

    image comparison slider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import ImageSlider from 'react-image-comparison-slider'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: 700, height: 450 }}>
+      <ImageSlider
+        image1="https://images.pexels.com/photos/4474767/pexels-photo-4474767.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+        alt1=""
+        image2="https://images.pexels.com/photos/333850/pexels-photo-333850.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+        alt2=""
+        sliderColor="white"
+        handleColor="white"
+        handleBackgroundColor="black"
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
